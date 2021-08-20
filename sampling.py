ccdfcf1709fb69aaebc9a67e696db13ba1931bb5
leechanwoo <cjswosa2@gmail.com>
2021-08-21 01:01:25 +0900

    resize and write video, upload test

--- sampling.py
+++ sampling.py
@@ -2,12 +2,13 @@
 import cv2
 import boto3
 import sys
-sys.stdout.flush()
 
 bucket = 'panoramic-videos'
+out_bucket = 'extracted-panoramic-images'
 date = 17
 name = "NVR-CH01_S20210817-000000_E20210817-001334.mp4"
 video_path = f"{date}/{name}"
+out_path = f"test/{name}"
 download_path = "/home/videos/{}"
 downloaded_file = download_path.format(name)
 
@@ -19,22 +20,42 @@ s3.download_file(bucket, video_path, downloaded_file)
 print("cature created")
 cap = cv2.VideoCapture(downloaded_file)
 length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+fps = cap.get(cv2.cv.CV_CAP_PROP_FPS)
+width  = cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)   
+height = cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
 
-print(f"total frame counts: {length}")
-cap.release()
+out_name = "out_" + name
+frameSize = (int(width/4), int(height/4))
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+writer = cv2.VideoWriter(out_name, fourcc, fps, frameSize)
 
-exit()
-
-count = 0 
+print(f"org length: {length}")
+print(f"org fps: {fps}")
+print(f"org dimension: {width}, {height}")
+print(f"out dimension: {width/4}, {height/4}")
+print(f("out fps: fps"))
 
 print("capture start")
-for i in range(999999999):
+n = 20
+samples_interval = length/n
 
+for i in range(999999999):
     ret, frame = cap.read()
     if not ret:
-        count = i
         cap.release()
         break
-    print(f"count: {i}")
 
-print(f"frame count: {count}")
+    if not length % samples_interval == 0:
+        continue
+
+    resized = cv2.resize(frame, dsize=frameSize, interpolation=cv2.INTER_AREA)
+    writer.write(resized)
+    
+
+upload_file = "out.mp4"
+s3.upload_file(out_bucket, out_path, upload_file)
+
+
+cap.release()
+writer.release()
+
