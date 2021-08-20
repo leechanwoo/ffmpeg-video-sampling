4a4d4a4f5db7d2b4f26ba3a6f4ae1a03a655996d
leechanwoo <cjswosa2@gmail.com>
2021-08-20 23:51:22 +0900

    insert helpful logs

--- sampling.py
+++ sampling.py
@@ -1,6 +1,8 @@
 
 import cv2
 import boto3
+import sys
+sys.stdout.flush()
 
 bucket = 'panoramic-videos'
 date = 17
@@ -10,21 +12,24 @@ download_path = "/home/videos/{}"
 downloaded_file = download_path.format(name)
 
 s3 = boto3.client('s3')
+
+print(f"download file: {video_path}")
 s3.download_file(bucket, video_path, downloaded_file)
 
+print("cature created")
 cap = cv2.VideoCapture(downloaded_file)
-
 
 count = 0 
 
+print("capture start")
 for i in range(999999999):
 
     ret, frame = cap.read()
     if not ret:
         count = i
-        print(count, flush=True)
+        print(count)
         cap.release()
         break
 
 
-print(f"frame count: {count}", flush=True)
+print(f"frame count: {count}")
