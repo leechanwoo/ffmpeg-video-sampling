2ea0354bacad87fe4254a166006d8454769bcd44
leechanwoo <cjswosa2@gmail.com>
2021-08-21 01:20:38 +0900

    test cli progress

--- sampling.py
+++ sampling.py
@@ -47,10 +47,12 @@ for i in range(999999999):
         writer.release()
         break
 
+    print(f"\r{i}th frame processed")
+
     if not length % samples_interval == 0:
         continue
     
-    print(f"{i}/{length}th, {i % length}/{n} frame written")
+    #print(f"{i}/{length}th, {i % length}/{n} frame written")
     resized = cv2.resize(frame, dsize=frameSize, interpolation=cv2.INTER_AREA)
     writer.write(resized)
     
