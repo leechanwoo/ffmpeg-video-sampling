e6b98fb3e41c5ba8fece58a3d44592fd73f84c9a
leechanwoo <cjswosa2@gmail.com>
2021-08-20 22:47:50 +0900

    download test from s3

--- sampling.py
+++ sampling.py
@@ -3,9 +3,14 @@ import cv2
 import time 
 
 
-for i in range(10):
-    print(i)
-    time.sleep(1)
+bucket = 'panoramic-videos'
+date = 17
+name = "NVR-CH01_S20210817-000000_E20210817-001334.mp4"
+video_path = f"{date}/{name}"
+
+s3 = boto3.client('s3')
+s3.download_file(bucket, video_path, "./videos")
 
 
-print("sampling runs")
+
+print("download complete")
