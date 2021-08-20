8f7e33bb453503c6acf5194396d23c0ba00513ca
leechanwoo <cjswosa2@gmail.com>
2021-08-20 23:03:59 +0900

    frame count test

--- sampling.py
+++ sampling.py
@@ -1,16 +1,28 @@
 
 import cv2
-import time 
 import boto3
 
 bucket = 'panoramic-videos'
 date = 17
 name = "NVR-CH01_S20210817-000000_E20210817-001334.mp4"
 video_path = f"{date}/{name}"
+download_path = "/home/videos/{}"
+downloaded_file = download_path.format(name)
 
 s3 = boto3.client('s3')
-s3.download_file(bucket, video_path, f"/home/videos/{name}")
+s3.download_file(bucket, video_path, downloaded_file)
+
+cap = cv2.VideoCapture(downloaded_file)
 
 
+count = 0 
 
-print("download complete")
+for i in range(999999999):
+
+    ret, frame = cap.read()
+    if not ret:
+        count = i
+        break
+
+
+print(f"frame count: {count}")
