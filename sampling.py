97ad7a44b932c9d0e13e37fc796d29a92ba8ae28
leechanwoo <cjswosa2@gmail.com>
2021-08-20 22:59:25 +0900

    absolute path

--- sampling.py
+++ sampling.py
@@ -9,7 +9,7 @@ name = "NVR-CH01_S20210817-000000_E20210817-001334.mp4"
 video_path = f"{date}/{name}"
 
 s3 = boto3.client('s3')
-s3.download_file(bucket, video_path, "./videos")
+s3.download_file(bucket, video_path, f"/home/videos/{name}")
 
 
 
