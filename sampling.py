1ef4850d9814244f5132fa84ce9472d8e5b32162
leechanwoo <cjswosa2@gmail.com>
2021-08-22 03:25:32 +0900

    image sampling test

--- sampling.py
+++ sampling.py
@@ -14,7 +14,9 @@ class VideoSampler():
         self.dst_bucket = config.dst_bucket
         
 
-    def process(self, video_name, out_name):
+    def process(self, video_key):
+
+        video_path, video_name = tuple(video_key.split('/'))
 
         print("cature created")
         cap = cv2.VideoCapture(video_name)
@@ -23,9 +25,9 @@ class VideoSampler():
         width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   
         height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-        frameSize = (int(width/4), int(height/4))
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        writer = cv2.VideoWriter(out_name, fourcc, fps, frameSize)
+        #  frameSize = (int(width/4), int(height/4))
+        #  fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        #  writer = cv2.VideoWriter(out_name, fourcc, fps, frameSize)
 
         print(f"org length: {length}")
         print(f"org fps: {fps}")
@@ -35,21 +37,31 @@ class VideoSampler():
 
         print("capture start")
 
-        for i in range(1000):
+        for i in range(999999999):
+            for j in range(29):
+                ret, dummy = cap.read()
+                del dummy
+                if not ret:
+                    break
+
             ret, frame = cap.read()
             if not ret:
                 cap.release()
-                writer.release()
+                #  writer.release()
                 break
 
             progress = int(i/length*100)
             print(f"{i}/{length} {progress}% [" + "#"*progress + " "*(100-progress)+ "]")
 
-            resized = cv2.resize(frame, dsize=frameSize, interpolation=cv2.INTER_AREA)
-            writer.write(resized)
+            image_name = video_name + f'_{i}'.zfill(6) + '.jpg'
+            cv2.imwrite(image_name, frame)
+            #  resized = cv2.resize(frame, dsize=frameSize, interpolation=cv2.INTER_AREA)
+            #  writer.write(resized)
 
+            upload_path = os.path.join(video_path, image_name)
+            self.upload_image(upload_path)
             
-        writer.release()
+        #  writer.release()
         cap.release()
 
         print("release")
@@ -75,57 +87,42 @@ class SamplerConfig():
 
 
 if __name__ == "__main__":
-
-    
     ay_rng = "202108(0[3-6]|09|1[0-2])-[0-1][7-9]\d\d\d\d"
     jj_rng = "2021\d\d\d\d[0-1][7-9]\d\d\d\d"
-    #  ch1 = re.compile(f"NVR-CH01_S2021\d\d\d\d-\d\d\d\d\d\d_E2021\d\d\d\d-\d\d\d\d\d\d\.(avi|mp4)")
-    #  ch2 = re.compile(f"NVR-CH02_S2021\d\d\d\d-\d\d\d\d\d\d_E2021\d\d\d\d-\d\d\d\d\d\d\.(avi|mp4)")
-    #  jj = re.compile("_2021\d\d\d\d\d\d\d\d\d\d_2021\d\d\d\d\d\d\d\d\d\d_0(|_1)\.avi")
 
     ch1 = re.compile(f"NVR-CH01_S{ay_rng}_E2021\d\d\d\d-\d\d\d\d\d\d\.(avi|mp4)")
     ch2 = re.compile(f"NVR-CH02_S{ay_rng}_E2021\d\d\d\d-\d\d\d\d\d\d\.(avi|mp4)")
     jj = re.compile(f"_{jj_rng}_2021\d\d\d\d\d\d\d\d\d\d_0(|_1)\.avi")
     
     config = SamplerConfig()
-    src_bucket = config.src_bucket
 
-    objs = list(src_bucket.objects.all())
+    objs = list(config.src_bucket.objects.all())
     print(f"Number of objects: {len(objs)}")
 
-    def match(fn):
+    def regex(fn):
         def get_name(summ):
             return fn(summ.key.split('/')[-1])
         return get_name
 
-    rush_hours = list(filter(match(ch1.match), objs)) +  list(filter(match(ch2.match), objs)) +  list(filter(match(jj.search), objs))
-
-    for o in rush_hours:
-        print(o.key)
+    rush_hours = list(filter(regex(ch1.match), objs)) +  list(filter(regex(ch2.match), objs)) +  list(filter(regex(jj.search), objs))
 
 
-    #  filtered = list(filter(lambda summ: ch1.match(get_name(summ)), objs))
-    #  filtered = list(filter(lambda summ: ch2.match(summ.key.split('/')[-1]), objs))
-    #  filtered = list(filter(lambda summ: jj.search(summ.key.split('/')[-1]), objs))
-
-    exit()
-
-
-    config = SamplerConfig()
     svc = VideoSampler(config=config)
 
 
-    video_name = "NVR-CH01_S20210817-000000_E20210817-001334.mp4"
-    out_name = "out_" + video_name
+    #  video_name = "NVR-CH01_S20210817-000000_E20210817-001334.mp4"
+    #  out_name = "out_" + video_name
+
+    for key in rush_hours[:1]:
+
+        #  svc.download_video(f"{17}/{video_name}")
+        svc.download_video(key)
+
+        svc.process(video_name=video_name)
+
+        #  svc.upload_image(f"test/{out_name}")
 
 
-    svc.download_video(f"{17}/{video_name}")
-
-    svc.process(video_name=video_name, out_name=out_name)
-
-    svc.upload_image(f"test/{out_name}")
-
-
-    os.remove(video_name)
-    os.remove(out_name)
+        os.remove(video_name)
+        os.remove(out_name)
 
