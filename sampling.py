d4fbd5e4a11644f99c095bb5c89f6c6b664f1daa
leechanwoo <cjswosa2@gmail.com>
2021-08-21 22:58:19 +0900

    refactoring

--- sampling.py
+++ sampling.py
@@ -7,67 +7,92 @@ import sys
 sys.stdout.flush()
 
 
-def process(video_name, out_name):
-
-    print("cature created")
-    cap = cv2.VideoCapture(video_name)
-    length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   
-    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-
-    frameSize = (int(width/4), int(height/4))
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    writer = cv2.VideoWriter(out_name, fourcc, fps, frameSize)
-
-    print(f"org length: {length}")
-    print(f"org fps: {fps}")
-    print(f"org dimension: {width}, {height}")
-    print(f"out dimension: {width/4}, {height/4}")
-    print(f"out fps: {fps}")
-
-    print("capture start")
-
-    for i in range(100):
-        ret, frame = cap.read()
-        if not ret:
-            cap.release()
-            writer.release()
-            break
-
-        progress = int(i/length*100)
-        print(f"{i}/{length} {progress}% [" + "#"*progress + " "*(100-progress)+ "]")
-
-        resized = cv2.resize(frame, dsize=frameSize, interpolation=cv2.INTER_AREA)
-        writer.write(resized)
-
+class VideoSampler():
+    def __init__(self, s3, src_bucket_name, dst_bucket_name):
+        self.src_bucket = s3.Bucket(src_bucket_name)
+        self.dst_bucket = s3.Bucket(dst_bucket_name)
         
-    writer.release()
-    cap.release()
 
-    print("release")
+    def process(video_name, out_name):
+
+        print("cature created")
+        cap = cv2.VideoCapture(video_name)
+        length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   
+        height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+        frameSize = (int(width/4), int(height/4))
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        writer = cv2.VideoWriter(out_name, fourcc, fps, frameSize)
+
+        print(f"org length: {length}")
+        print(f"org fps: {fps}")
+        print(f"org dimension: {width}, {height}")
+        print(f"out dimension: {width/4}, {height/4}")
+        print(f"out fps: {fps}")
+
+        print("capture start")
+
+        for i in range(100):
+            ret, frame = cap.read()
+            if not ret:
+                cap.release()
+                writer.release()
+                break
+
+            progress = int(i/length*100)
+            print(f"{i}/{length} {progress}% [" + "#"*progress + " "*(100-progress)+ "]")
+
+            resized = cv2.resize(frame, dsize=frameSize, interpolation=cv2.INTER_AREA)
+            writer.write(resized)
+
+            
+        writer.release()
+        cap.release()
+
+        print("release")
+
+    def download_video(download_path):
+        self.src_bucket.download_file(download_path, self._get_name(download_path))
+        print(f"{download_path} downloaded")
+    
+    def upload_image(upload_path):
+        self.dst_bucket.upload_file(self._get_name(upload_path), upload_path)
+        print(f"{upload_path} uploaded")
+
+
+    def _get_name(path):
+        return path.split('/')[-1]
+
+
+
+
 
 
 
 
 if __name__ == "__main__":
     date = 17
-    video_name = "NVR-CH01_S20210817-000000_E20210817-001334.mp4"
-    out_name = "out_" + video_name
 
     s3 = boto3.resource('s3')
 
-    src_bucket = s3.Bucket('panoramic-videos')
+    svc = VideoSampler( s3=s3,
+                        src_bucket_name='panoramic-videos', 
+                        dst_bucket_name='extracted-panoramic-images')
+
+
+    video_name = "NVR-CH01_S20210817-000000_E20210817-001334.mp4"
     download_path = f"{date}/{video_name}"
-    src_bucket.download_file(download_path, video_name)
-    print(f"{video_name} downloaded")
+    svc.download_video(download_path)
 
-    process(video_name=video_name, out_name=out_name)
 
-    smpl_bucket = s3.Bucket('extracted-panoramic-images')
+    svc.process(video_name=video_name, out_name=out_name)
+
+
+    out_name = "out_" + video_name
     upload_path = f"test/{out_name}"
-    smpl_bucket.upload_file(out_name, upload_path)
-    print(f"{out_name} uploaded")
+    svc.upload_image(upload_path)
 
 
     os.remove(video_name)
