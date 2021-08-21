972b97a17b18bb53cef92f001eddd92dd7a986f5
leechanwoo <cjswosa2@gmail.com>
2021-08-21 21:43:17 +0900

    download at current path

--- sampling.py
+++ sampling.py
@@ -9,13 +9,13 @@ bucket = 'panoramic-videos'
 date = 17
 name = "NVR-CH01_S20210817-000000_E20210817-001334.mp4"
 video_path = f"{date}/{name}"
-download_path = "/home/videos/{}"
-downloaded_file = download_path.format(name)
+#download_path = "/home/videos/{}"
+#downloaded_file = download_path.format(name)
 
 s3 = boto3.client('s3')
 
 print(f"download file: {video_path}")
-s3.download_file(bucket, video_path, downloaded_file)
+s3.download_file(bucket, video_path, name)
 
 print("cature created")
 cap = cv2.VideoCapture(downloaded_file)
